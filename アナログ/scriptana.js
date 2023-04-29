278268034d136567996227beceee61c98ae94b52
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2023-04-29 16:27:20 +0900

    記録

---
+++ アナログ/scriptana.js
@@ -0,0 +1,13 @@
+function time(){
+    var date = new.Date();
+    var h = new.getHours();
+    var m = new.getMinutes();
+    var s = new.getSeconds();
+    var ms = date.getMillseconds();
+
+    var hdeg = h*30+m*0.5;
+    var mdeg = m*6+s*0.1;
+    var sdeg = s*6+ms*0.006;
+
+    document.
+}
