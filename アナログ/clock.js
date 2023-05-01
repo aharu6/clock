e2803f00c9a9c8f997e6fe7ee23b8a68eb32a9dc
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2023-05-01 21:56:19 +0900

    記録

--- アナログ/clock.js
+++ アナログ/clock.js
@@ -1,58 +1,17 @@
-window.onload = function () {
-  showananalogclock();
-  setInterval(updateclock, 1000);
-  if (time < 10) {
-    return "0" + time;
-  } else {
-    return time;
-  }
-};
-let now = new Date();
-let hours = now.getHours();
-if (hours >= 6 && hours < 10) {
-  showananalogclock();
-} else {
-  showdegitalclock();
-}
-//アナログ表示
-function showananalogclock() {
-  document.getElementById("analog-clock").style.display = "block";
-  document.getElementById("degital-clock").style.display = "none";
-  analogclock.classlist.add("show");
-  analogclock.classlist.remove("hide");
-}
-//デジタル表示
-function showdegitalclock() {
-  document.getElementById("analog-clock").style.display = "none";
-  document.getElementById("degital-clock").style.display = "block";
-  degitalclock.classlist.add("show");
-  degitalclock.classlist.remove("hide");
-}
-//各クラス取得中のリアルタイムでの時間表示　デジタルとアナログそれぞれ
-function updateclock() {
-  let now = new Date();
-  if (document.getElementById("analog-clock").classList.contains("show")) {
-    analogclock(now);
-  } else if (
-    document.getElementById("degital-clock").classList.contains("show")
-  ) {
-    degitalclock(now);
-  }
-}
 //時計本体 アナログ
 function analogclock() {
-  var now = new Date();
-  var hours = now.getHours();
-  var minutes = now.getMinutes();
-  var seconds = now.getSeconds();
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
 
-  var hourhand = document.querySelector(".hour-hand");
-  var minutehand = document.querySelector(".minutes-hand");
-  var secondhand = document.querySelector(".seconds-hand");
+  let hourhand = document.querySelector(".hour-hand");
+  let minutehand = document.querySelector(".minutes-hand");
+  let secondhand = document.querySelector(".seconds-hand");
 
-  var hourhanddeg = (hours % 12) * 30 + (minutes / 60) * 30;
-  var minutshanddeg = (minutes / 60) * 360;
-  var secondhanddeg = (seconds / 60) * 360;
+  let hourhanddeg = (hours % 12) * 30 + (minutes / 60) * 30;
+  let minutshanddeg = (minutes / 60) * 360;
+  let secondhanddeg = (seconds / 60) * 360;
 
   hourhand.style.transform = "rotate(" + hourhanddeg + "deg)";
   minutehand.style.transform = "rotate(" + minutshanddeg + "deg)";
@@ -70,5 +29,43 @@ function degitalclock() {
     addZeroPadding(minutes) +
     ":" +
     addZeroPadding(seconds);
-  document.getElementById("degital-clock").innerHTML = timeString;
+  document.getElementById("degitalclock").innerHTML = timeString;
 }
+
+//アナログ表示
+function showananalogclock() {
+  document.getElementById("analogclock").style.display = "block";
+  document.getElementById("degitalclock").style.display = "none";
+
+  analogclock.classlist.add("show");
+  analogclock.classlist.remove("hide");
+  hourhand.classlist.add("show");
+  hourhand.classList.remove("hide");
+  minuteshand.classList.add("show");
+  minuteshand.classList.remove("hide");
+  secondshand.classList.add("show");
+  secondshand.classList.remove("hide");
+  clockface.classList.add("show");
+  clockface.classListr.remove("hide");
+}
+//デジタル表示
+function showdegitalclock() {
+  document.getElementById("analogclock").style.display = "none";
+  document.getElementById("degitalclock").style.display = "block";
+  degitalclock.classlist.add("show");
+  degitalclock.classlist.remove("hide");
+}
+//各クラス取得中のリアルタイムでの時間表示　デジタルとアナログそれぞれ
+function updatetime() {
+  let now = new Date();
+  let minutes = now.getMinutes();
+
+  if (minutes % 10 === 0) {
+    showananalogclock();
+    analogclock();
+  } else {
+    
+    degitalclock();
+  }
+}
+setInterval(updatetime, 1000);
